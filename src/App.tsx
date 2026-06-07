4142a53fa57b137b8a6a74f1de83f4078fb76d6d
Emre Can Çorapçı <emrecancorapci@gmail.com>
2026-06-08 01:37:27 +0300

    feat: update app to add sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { Contact } from "./components/sections/contact";
+import { Hero } from "./components/sections/hero";
+import { Navbar } from "./components/sections/navbar";
+import { Products } from "./components/sections/products";
+import { Story } from "./components/sections/story";
+
 function App() {
-	return (
-		<div>
-		</div>
-	);
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <Hero />
+        <Story />
+        <Products />
+        <Contact />
+      </main>
+    </div>
+  );
 }
 
 export default App;
