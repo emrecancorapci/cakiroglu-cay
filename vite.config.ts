722dfb8e237e7ccd0b14dbb8815dbbcf06d4328a
Emre Can Çorapçı <emrecancorapci@gmail.com>
2026-06-28 12:42:51 +0300

    fix: comment out the plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteHtmlPlugin } from "./generate-index";
+// import { viteHtmlPlugin } from "./generate-index";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,27 +14,27 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    viteHtmlPlugin({
-      htmlClass: "scroll-smooth bg-background",
-      title: "Çakıroğlu Çay",
-      description: "Damak Zevkinize Göre",
-      author: "Çakıroğlu Çay",
-      iconPath: "/images/logo-clear.svg",
-      keywords: ["çay", "çakıroğlu", "çakıroğlu çay"],
-      openGraph: {
-        title: "Çakıroğlu Çay - Damak Zevkinize Göre",
-        description:
-          "Karadeniz'in dik yamaçlarından elle toplanmış ve mineral bakımından zengin topraklarda yetiştirilen *Çakıroğlu Çay*, mükemmel demlenmiş bir fincan çayın koyu kırmızı rengini ve keskin, saf lezzetini sunar.",
-        image: "/images/logo-black-gold-close.jpg",
-        url: "",
-      },
-      twitter: {
-        title: "Çakıroğlu Çay - Damak Zevkinize Göre",
-        description:
-          "Karadeniz'in dik yamaçlarından elle toplanmış ve mineral bakımından zengin topraklarda yetiştirilen *Çakıroğlu Çay*, mükemmel demlenmiş bir fincan çayın koyu kırmızı rengini ve keskin, saf lezzetini sunar.",
-        image: "/images/logo-black-gold-close.jpg",
-        site: "",
-      },
-    }),
+    // viteHtmlPlugin({
+    //   htmlClass: "scroll-smooth bg-background",
+    //   title: "Çakıroğlu Çay",
+    //   description: "Damak Zevkinize Göre",
+    //   author: "Çakıroğlu Çay",
+    //   iconPath: "/images/logo-clear.svg",
+    //   keywords: ["çay", "çakıroğlu", "çakıroğlu çay"],
+    //   openGraph: {
+    //     title: "Çakıroğlu Çay - Damak Zevkinize Göre",
+    //     description:
+    //       "Karadeniz'in dik yamaçlarından elle toplanmış ve mineral bakımından zengin topraklarda yetiştirilen *Çakıroğlu Çay*, mükemmel demlenmiş bir fincan çayın koyu kırmızı rengini ve keskin, saf lezzetini sunar.",
+    //     image: "/images/logo-black-gold-close.jpg",
+    //     url: "",
+    //   },
+    //   twitter: {
+    //     title: "Çakıroğlu Çay - Damak Zevkinize Göre",
+    //     description:
+    //       "Karadeniz'in dik yamaçlarından elle toplanmış ve mineral bakımından zengin topraklarda yetiştirilen *Çakıroğlu Çay*, mükemmel demlenmiş bir fincan çayın koyu kırmızı rengini ve keskin, saf lezzetini sunar.",
+    //     image: "/images/logo-black-gold-close.jpg",
+    //     site: "",
+    //   },
+    // }),
   ],
 });
